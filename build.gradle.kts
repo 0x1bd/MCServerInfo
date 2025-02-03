5c919f5ec066286c1a0679ce6b9cdd5445dc904e
meo209 <redslyice@outlook.de>
2025-02-03 18:00:10 +0100

    fix: jitpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,3 +26,25 @@ tasks.test {
 kotlin {
     jvmToolchain(21)
 }
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["kotlin"])
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+
+            pom {
+                name.set("MCServerInfo")
+                description.set("Ping minecraft servers and get information's about them.")
+                url.set("https://github.com/0x1bd/MCServerInfo")
+            }
+        }
+    }
+    repositories {
+        maven {
+            url = uri("https://jitpack.io")
+        }
+    }
+}
